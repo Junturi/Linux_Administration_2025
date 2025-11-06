2a5c9e663a6802e462986549a4a4226c891c7c72
Junturi <juntunen.riikka@outlook.com>
2025-11-06 17:18:21 +0200

    Added a bash script for updating files automatically

---
+++ update_git.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Pulling latest changes..."
+sudo git pull
+
+echo "Copying updated files..."
+sudo cp apache/index.html /var/www/html/index.html
+sudo cp nginx/index.html /var/www/nginx/index.html
+sudo cp app.py ~/lemp-app/app.py
+
+echo "Restarting apache"
+sudo systemctl restart apache2
+
+echo "Restarting nginx"
+sudo systemctl restart nginx
+
+echo "Update complete."
