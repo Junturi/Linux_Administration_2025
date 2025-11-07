8ff9f3a731d9bf7e511888e721586d472f9843ca
Junturi <juntunen.riikka@outlook.com>
2025-11-07 14:26:00 +0200

    Fixed missing import in app.py

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo cp apache/index.html /var/www/html/index.html
 sudo cp nginx/index.html /var/www/nginx/index.html
 
 sudo cp lemp-app/app.py ~/lemp-app/app.py
-sudo cp -ra lemp-app/templates/ ~/lemp-app/templates/
+sudo cp -ra lemp-app/templates/ ~/lemp-app/templates
 
 sudo cp lemp-app_config/lemp-app /etc/nginx/sites-available/lemp-app
 sudo cp lemp-app_config/lemp-app.service /etc/systemd/system/lemp-app.service
@@ -21,6 +21,8 @@ sudo systemctl restart apache2
 
 echo "Restarting nginx"
 sudo systemctl restart nginx
+
+echo "Restart lemp-app"
 sudo systemctl daemon-reload
 sudo systemctl restart lemp-app
 
--- lemp-app/app.py
+++ lemp-app/app.py
@@ -1,6 +1,5 @@
-from flask import Flask
-import mysql.connector
-import os
+from flask import Flask, render_template
+import mysql.connector, os
 from dotenv import load_dotenv
 
 load_dotenv()
