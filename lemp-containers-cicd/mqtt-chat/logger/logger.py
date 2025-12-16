62e62a1c8e4c663387b9353b0dfe94958ab67142
Junturi <juntunen.riikka@outlook.com>
2025-12-16 10:31:47 +0200

    Lisätty DB_HOST .env-tiedostoon

--- lemp-containers-cicd/mqtt-chat/logger/logger.py
+++ lemp-containers-cicd/mqtt-chat/logger/logger.py
@@ -17,7 +17,7 @@ MQTT_PORT = 1883
 MQTT_TOPIC = "chat/messages"
 
 DB_CONFIG = {
-    "host": os.getenv('DB_HOST', 'db'),
+    "host": os.getenv('DB_HOST'),
     "user": os.getenv('DB_USER'),
     "password": os.getenv('DB_PASSWORD'),
     "database": os.getenv('DB_NAME')
